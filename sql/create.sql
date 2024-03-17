10ec4b61f667e8e2a9138e2362e68a21066e006e
taegon77 <152164762+taegon77@users.noreply.github.com>
2024-03-17 13:26:38 +0900

    SQL) create table

--- sql/create.sql
+++ sql/create.sql
@@ -1,1 +1,15 @@
+create database user(
+    id varchar(60),
+    name varchar(20),
+    profileImg varchar(200),
+    created datetime not null default current_timestamp(),
+    updated datetime not null default current_timestamp() on update current_timestamp(),
+    primary key (id)
+);
 
+insert into user(id, name, profileImg) values
+('a','ㄱ',''),
+('b','ㄴ',''),
+('c','ㄷ',''),
+('d','ㄹ',''),
+('e','ㅁ','');
